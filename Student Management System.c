30ae4e3caf4bcd5eddde9cf287ca004ab3a92273
JJX <942180455@qq.com>
2025-07-07 21:13:17 +0800

    2025年7月7日练习

---
+++ Student Management System.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+    // 定义结构体，应该在函数外或者头文件中
+    struct Student
+    {
+        int id;
+        char name[100];
+        int age;
+        float score;
+    };
+
+// 输入学生信息
+void inputStudentInfo(int len, struct Student *pArr)
+{
+    int i;
+    // struct Student
+    // {
+    //     int id;
+    //     char name[100];
+    //     int age;
+    //     float score;
+    // };
+
+    // // 动态构造一维数组
+    // printf("请输入你要存放的学生信息的个数：");
+    // printf("len = ");
+    // scanf("%d", &len);
+    // struct Student *pArr = (struct Student *)malloc(len * sizeof(struct Student)); // 动态的构造了一个一维数组, 该一维数组的长度是len，数组名是students。每个元素是struct Student，类似于struct Student students[len];
+    
+    printf("请输入学生信息：\n");
+    for ( i = 0; i < len; i++)
+    {
+        printf("第%d个学生的信息：\n", i + 1);
+        printf("ID：");
+        scanf("%d", &pArr[i].id);
+        printf("姓名：");
+        scanf("%s", pArr[i].name);
+        printf("年龄：");
+        scanf("%d", &pArr[i].age);
+        printf("成绩：");
+        scanf("%f", &pArr[i].score);
+    }
+    
+}
+
+// 打印学生信息
+void printStudentInfo(int len, struct Student *pArr)
+{
+    int i;
+
+    for ( i = 0; i < len; i++)
+    {
+        printf("------------------------\n");
+        printf("学生信息为：\n");
+        printf("ID：%d\n", pArr[i].id);
+        printf("姓名：%s\n", pArr[i].name);
+        printf("年龄：%d\n", pArr[i].age);
+        printf("成绩：%.2f\n", pArr[i].score);
+        printf("------------------------\n");
+    }
+}
+
+// 排序学生信息
+// 使用冒泡排序
+void sort(int len, struct Student *pArr)
+{
+    int i, j;
+    struct Student temp;
+
+    for ( i = 0; i < len - 1; i++)
+    {
+        for ( j = 0; j < len - 1 - i; j++)
+        {
+            if (pArr[j].score > pArr[j + 1].score) // 按照成绩从小到大排序 降序就换成小于号
+            {
+                temp = pArr[j];
+                pArr[j] = pArr[j + 1];
+                pArr[j + 1] = temp;
+            }
+        }
+    }
+    
+    
+}
+
+// 主函数，尽量简洁
+int main(void)
+{
+    int len;
+
+    // 动态构造一维数组
+    printf("请输入你要存放的学生信息的个数：");
+    printf("len = ");
+    scanf("%d", &len);
+    struct Student *pArr = (struct Student *)malloc(len * sizeof(struct Student)); // 动态的构造了一个一维数组, 该一维数组的长度是len，数组名是students。每个元素是struct Student，类似于struct Student students[len];
+
+    if (pArr == NULL) // 检查内存分配是否成功
+    {
+        printf("内存分配失败！\n");
+        return 1; // 返回非零值表示错误
+    }
+
+    // printf("size%d", sizeof(struct Student));
+    // printf("size%d", sizeof(pArr[1].name));
+    // printf("size%d", sizeof(int));
+    // printf("size%d", sizeof(float));
+
+    // 输入学生信息
+    inputStudentInfo(len, pArr);
+    // 排序学生信息
+    sort(len, pArr);
+    // 打印学生信息
+    printStudentInfo(len, pArr);
+
+    free(pArr); // 释放动态分配的内存
+    return 0;
+}
--- practice.c
+++ practice.c
@@ -1156,37 +1156,81 @@
 // }
 
 
-#include <stdio.h>
-#include <string.h>
+// 结构体
+// #include <stdio.h>
+// #include <string.h>
 
-struct Student
-{
-    char name[100];
-    int age;
-    char sex;
-    float score;
-}; // 分号不能省略
-// 前面这一块叫结构体 
+// struct Student
+// {
+//     char name[100];
+//     int age;
+//     char sex;
+//     float score;
+// }; // 分号不能省略
+// // 前面这一块叫结构体 
 
-int main(void)
-{
-    struct Student st;
+// int main(void)
+// {
+//     struct Student st;
     
-    // struct Student st = // 这个叫结构体变量
-    // {
-    //     "张三",
-    //     20,
-    //     'M',
-    //     100.0
-    // };
-    struct Student * pst = &st;
+//     // struct Student st = // 这个叫结构体变量
+//     // {
+//     //     "张三",
+//     //     20,
+//     //     'M',
+//     //     100.0
+//     // };
+//     struct Student * pst = &st;
 
-    pst -> age = 20;
-    strcpy(pst -> name, "张三");
-    st.sex = 'F';
+//     pst -> age = 20;
+//     strcpy(pst -> name, "张三");
+//     st.sex = 'F';
 
-    printf("%d %c %s", st.age, st.sex, st.name);
+//     printf("%d %c %s", st.age, st.sex, st.name);
     
      
-    return 0;
-}
+//     return 0;
+// }
+
+
+
+// #include <stdio.h>
+
+// void sort(int * arr, int len)
+// {
+//     int i, j, t;
+
+//     for ( i = 0; i < len - 1; i++)
+//     {
+//         for ( j = i + 1; j < len; j++)
+//         {
+//             if (arr[i] < arr[j])
+//             {
+//                 t = arr[i];
+//                 arr[i] = arr[j];
+//                 arr[j] = t; // 交换两个元素的值
+//             }
+//         }
+//     }
+// }
+
+// int main(void)
+// {
+//     int arr[6] = {5, 3, 6, 7, 5, 4};
+//     int len = sizeof(arr) / sizeof(arr[0]); //计算数组的长度
+//     int i;
+
+//     printf("%d\n", arr);
+//     printf("%d\n", sizeof(arr));
+//     printf("%d\n", arr[0]);
+//     printf("%d\n", sizeof(arr[0]));
+
+//     sort(arr, len);
+//     printf("排序后的数组为：\n");
+//     for (i = 0; i < len; i++)
+//     {
+//         printf("%d ", arr[i]);
+//     }
+    
+//     return 0;
+// }
