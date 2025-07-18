f4c7df9051fbb2b264c7aaaf3f6f9b0a153dc747
JJX <942180455@qq.com>
2025-07-18 17:41:42 +0800

    2025年7月18日

--- practice.c
+++ practice.c
@@ -1358,11 +1358,55 @@
 // }
 
 
+// #include <stdio.h>
+
+// void swap(int *a, int *b)
+// {
+//     int p;
+//     p = *a;
+//     *a = *b;
+//     *b = p;
+// }
+
+// int main(void)
+// {
+//     int a = 1;
+//     int b = 2;
+//     swap(&a, &b);
+//     printf("%d\n%d", a, b);
+
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+ 
+// int main ()
+// {
+//    int  var = 20;   /* 实际变量的声明 */
+//    int  *ip;        /* 指针变量的声明 */
+ 
+//    ip = &var;  /* 在指针变量中存储 var 的地址 */
+ 
+//    printf("var 变量的地址: %p\n", &var  );
+ 
+//    /* 在指针变量中存储的地址 */
+//    printf("ip 变量存储的地址: %p\n", ip );
+ 
+//    /* 使用指针访问值 */
+//    printf("*ip 变量的值: %d\n", *ip );
+ 
+//    return 0;
+// }
+
+
 #include <stdio.h>
 
 int main(void)
 {
-    
+    int a = 1;
+    printf("%x\n", a);
+    printf("%p\n", &a);
 
     return 0;
 }
